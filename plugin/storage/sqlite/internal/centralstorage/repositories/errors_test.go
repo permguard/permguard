86c603ec282f5499b8a706742d226a6cb176e22f
Nicola Gallo <nicolagmt@hotmail.com>
2024-08-20 18:27:05 +0200

    feat: working on sqlite refactgoring

---
+++ plugin/storage/sqlite/internal/centralstorage/repositories/errors_test.go
@@ -0,0 +1,63 @@
+// Copyright 2024 Nitro Agility S.r.l.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package repositories
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/mattn/go-sqlite3"
+	"github.com/stretchr/testify/assert"
+
+	azerrors "github.com/permguard/permguard/pkg/extensions/errors"
+)
+
+
+func TestWrapSqlite3Error(t *testing.T) {
+	tests := map[string]struct {
+		ErrorIn error
+		ErrorOut error
+	}{
+		"here a sample error 1": {
+			errors.New("here a sample error 1"),
+			azerrors.WrapSystemError(azerrors.ErrStorageConstraintUnique, "storage: constraint failed - here a sample error 1"),
+		},
+		"here a sample error 2": {
+			sqlite3.Error{Code: sqlite3.ErrConstraint },
+			azerrors.WrapSystemError(azerrors.ErrStorageConstraintUnique, "storage: constraint failed - here a sample error 2"),
+		},
+		"here a sample error 3": {
+			sqlite3.Error{Code: sqlite3.ErrConstraint, ExtendedCode: sqlite3.ErrConstraintUnique },
+			azerrors.WrapSystemError(azerrors.ErrStorageConstraintUnique, "storage: unique constraint failed - here a sample error 3"),
+		},
+		"here a sample error 4": {
+			sqlite3.Error{Code: sqlite3.ErrNotFound },
+			azerrors.WrapSystemError(azerrors.ErrStorageNotFound, "storage: record not found - here a sample error 4"),
+		},
+		"here a sample error 5": {
+			sqlite3.Error{Code: sqlite3.ErrAuth },
+			azerrors.WrapSystemError(azerrors.ErrStorageGeneric, "storage: generic error (here a sample error 5)"),
+		},
+	}
+	for message, test := range tests {
+		t.Run(message, func(t *testing.T) {
+			err := WrapSqlite3Error(message, test.ErrorIn)
+			assert.Error(t, err)
+			assert.True(t, azerrors.IsSystemError(test.ErrorOut))
+		})
+	}
+}
