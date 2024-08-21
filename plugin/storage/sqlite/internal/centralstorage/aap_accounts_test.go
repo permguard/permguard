7255c54160ab9ba32c1030266b163c2c8dde13e0
Nicola Gallo <nicolagmt@hotmail.com>
2024-08-21 16:55:23 +0200

    feat: working on sqlite refactgoring

--- plugin/storage/sqlite/internal/centralstorage/aap_accounts_test.go
+++ plugin/storage/sqlite/internal/centralstorage/aap_accounts_test.go
@@ -24,8 +24,8 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 
-	azmodels "github.com/permguard/permguard/pkg/agents/models"
 	azerrors "github.com/permguard/permguard/pkg/extensions/errors"
+	azmodels "github.com/permguard/permguard/pkg/agents/models"
 	azirepos "github.com/permguard/permguard/plugin/storage/sqlite/internal/centralstorage/repositories"
 )
 
