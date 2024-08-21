64338110a136375d9d744d2b924e30f82861950b
Nicola Gallo <nicolagmt@hotmail.com>
2024-08-21 18:17:52 +0200

    feat: working on sqlite refactgoring

--- internal/agents/services/pdp/controllers/pdp_local_controller.go
+++ internal/agents/services/pdp/controllers/pdp_local_controller.go
@@ -17,98 +17,12 @@
 package controllers
 
 import (
-	"encoding/json"
-	"errors"
-	"os"
-	"path/filepath"
-
 	azservices "github.com/permguard/permguard/pkg/agents/services"
-
-	azpermissions "github.com/permguard/permguard/pkg/accesscontrol/permissions"
-	azpolicies "github.com/permguard/permguard/pkg/accesscontrol/policies"
 )
 
 type PDPLocalController struct {
 	ctx   *azservices.ServiceContext
 	cache map[string]any
-}
-
-type papDoc struct {
-	Items []map[string]any `json:"items"`
-}
-
-func loadCache(cache *map[string]any, key string, targetKey string, encode bool, data *papDoc) error {
-	for _, item := range data.Items {
-		key := item[key].(string)
-		if !encode {
-			(*cache)[key] = item[targetKey]
-		} else {
-			bytes, err := json.Marshal(item[targetKey])
-			if err != nil {
-				return errors.Join(azservices.ErrServiceInvalidAppData, err)
-			}
-			(*cache)[key] = bytes
-		}
-	}
-	return nil
-}
-
-func loadCacheFromDisk(cache *map[string]any, appFolder string, key string, targetKey string, encode bool) error {
-	files, err := os.ReadDir(appFolder)
-	if err != nil {
-		return errors.Join(azservices.ErrServiceInvalidAppData, err)
-	}
-	for _, file := range files {
-		fileName := appFolder + file.Name()
-		if filepath.Ext(fileName) != ".json" {
-			continue
-		}
-		bArray, _ := os.ReadFile(fileName)
-		var data papDoc
-		err := json.Unmarshal(bArray, &data)
-		if err != nil {
-			return errors.Join(azservices.ErrServiceInvalidAppData, err)
-		}
-		err = loadCache(cache, key, targetKey, encode, &data)
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func (s *PDPLocalController) Setup() error {
-	var err error
-	s.cache = make(map[string]any)
-	err = loadCacheFromDisk(&s.cache, s.ctx.GetAppData()+"/identities/", "user_uur", "policies", false)
-	if err != nil {
-		return azservices.ErrServiceInvalidAppData
-	}
-	err = loadCacheFromDisk(&s.cache, s.ctx.GetAppData()+"/policies/", "policy_uur", "policy_payload", true)
-	if err != nil {
-		return azservices.ErrServiceInvalidAppData
-	}
-	return nil
-}
-
-func (s *PDPLocalController) GetPermissionsState(identityUUR azpolicies.UURString, settings ...azpermissions.PermissionsEngineOption) (*azpermissions.PermissionsState, error) {
-	engine, err := azpermissions.NewPermissionsEngine()
-	if err != nil {
-		return nil, err
-	}
-	policies := s.cache[string(identityUUR)]
-	if policies != nil {
-		for _, policy := range policies.([]any) {
-			registered, err := engine.RegisterPolicy(s.cache[policy.(string)].([]byte))
-			if err != nil {
-				return nil, err
-			}
-			if !registered {
-				return nil, azservices.ErrServiceGeneric
-			}
-		}
-	}
-	return engine.BuildPermissions(settings...)
 }
 
 func NewPDPLocalController(serviceContext *azservices.ServiceContext) (*PDPLocalController, error) {
