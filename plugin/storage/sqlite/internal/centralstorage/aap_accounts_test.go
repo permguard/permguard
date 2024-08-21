6540b3ab267d5888338686f5bede86f41501e01a
Nicola Gallo <nicolagmt@hotmail.com>
2024-08-21 15:32:44 +0200

    feat: working on sqlite refactgoring

--- plugin/storage/sqlite/internal/centralstorage/aap_accounts_test.go
+++ plugin/storage/sqlite/internal/centralstorage/aap_accounts_test.go
@@ -21,30 +21,61 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
 
 	azmodels "github.com/permguard/permguard/pkg/agents/models"
 	azerrors "github.com/permguard/permguard/pkg/extensions/errors"
 )
 
-// TestCreateAccountWithInvalidInputs tests the CreateAccount function with invalid inputs.
-func TestCreateAccountWithInvalidInputs(t *testing.T) {
-	assert := assert.New(t)
-	storage, _, _, _, _ := createSQLiteAAPCentralStorageWithMocks()
-	accounts, err := storage.CreateAccount(nil)
-	assert.Nil(accounts, "accounts should be nil")
-	assert.True(azerrors.AreErrorsEqual(azerrors.ErrClientParameter, err), "error should be errclientparameter")
-}
-
 // TestCreateAccountWithErrors tests the CreateAccount function with errors.
 func TestCreateAccountWithErrors(t *testing.T) {
 	assert := assert.New(t)
-	storage, storageCtx, mockConnector, _, mockSQLExec := createSQLiteAAPCentralStorageWithMocks()
 
-	errMsg := "Connect error"
-	inAccount := &azmodels.Account{}
-	mockSQLExec.On("Connect", storageCtx, mockConnector).Return(nil, errors.New(errMsg))
+	{ // Test with nil account
+		storage, _, _, _, _, _, _ := createSQLiteAAPCentralStorageWithMocks()
+		accounts, err := storage.CreateAccount(nil)
+		assert.Nil(accounts, "accounts should be nil")
+		assert.True(azerrors.AreErrorsEqual(azerrors.ErrClientParameter, err), "error should be errclientparameter")
+	}
 
-	accounts, err := storage.CreateAccount(inAccount)
-	assert.Nil(accounts, "accounts should be nil")
-	assert.EqualError(err, errMsg)
+	tests := map[string]struct {
+		IsCustomError bool
+		Error1 error
+	}{
+		"CONNECT-ERROR": { IsCustomError: true, Error1: azerrors.ErrStorageGeneric },
+		"BEGIN-ERROR": { IsCustomError: true, Error1: azerrors.ErrStorageGeneric },
+		"ROLLBACK-ERROR": { IsCustomError: false, Error1: errors.New("ROLLBACK-ERROR") },
+		"COMMIT-ERROR": { IsCustomError: true, Error1: azerrors.ErrStorageGeneric },
+	}
+	for testcase, test := range tests {
+		storage, mockStorageCtx, mockConnector, mockSQLRepo, mockSQLExec, sqlDB, mockSQLDB := createSQLiteAAPCentralStorageWithMocks()
+		inAccount := &azmodels.Account{}
+		switch testcase {
+		case "CONNECT-ERROR":
+			mockSQLExec.On("Connect", mockStorageCtx, mockConnector).Return(nil, errors.New(testcase))
+		case "BEGIN-ERROR":
+			mockSQLExec.On("Connect", mockStorageCtx, mockConnector).Return(sqlDB, nil)
+			mockSQLDB.ExpectBegin().WillReturnError(errors.New(testcase))
+		case "ROLLBACK-ERROR":
+			mockSQLExec.On("Connect", mockStorageCtx, mockConnector).Return(sqlDB, nil)
+			mockSQLDB.ExpectBegin()
+			mockSQLRepo.On("UpsertAccount", mock.Anything, true, mock.Anything).Return(nil, errors.New(testcase))
+		case "COMMIT-ERROR":
+			mockSQLExec.On("Connect", mockStorageCtx, mockConnector).Return(sqlDB, nil)
+			mockSQLDB.ExpectBegin()
+			mockSQLRepo.On("UpsertAccount", mock.Anything, true, mock.Anything).Return(nil, nil)
+			mockSQLDB.ExpectCommit().WillReturnError(errors.New(testcase))
+		default:
+			assert.FailNow("Unknown testcase")
+		}
+
+		accounts, err := storage.CreateAccount(inAccount)
+		assert.Nil(accounts, "accounts should be nil")
+		assert.Error(err)
+		if test.IsCustomError {
+			assert.True(azerrors.AreErrorsEqual(err, test.Error1), "error should be equal")
+		} else {
+			assert.Equal(test.Error1, err, "error should be equal")
+		}
+	}
 }
