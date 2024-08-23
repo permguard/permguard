b8cb69527ea9d19a0aba725fa55bd42a6b698a96
Nicola Gallo <nicolagmt@hotmail.com>
2024-08-23 15:13:44 +0200

    refactor: code refactoring to accomodate sqlite plugin changes

--- plugin/storage/sqlite/migrations/04_identities.sql
+++ plugin/storage/sqlite/migrations/04_identities.sql
@@ -32,63 +32,54 @@ CREATE INDEX identities_name_idx ON identities(name);
 CREATE INDEX identities_account_id_idx ON identities(account_id);
 CREATE INDEX identities_identity_source_id_idx ON identities(identity_source_id);
 
--- Creating the `identity_changestreams` table
-CREATE TABLE identity_changestreams (
-    changestream_id INTEGER NOT NULL PRIMARY KEY,
-	change_type TEXT NOT NULL,
-	change_at DATETIME DEFAULT CURRENT_TIMESTAMP NOT NULL,
-    identity_id TEXT NOT NULL,
-    created_at DATETIME NOT NULL,
-    updated_at DATETIME NOT NULL,
-    name TEXT NOT NULL,
-	kind INTEGER NOT NULL,
-	-- REFERENCES
-	account_id INTEGER NOT NULL,
-	identity_source_id TEXT NOT NULL
-);
-
-CREATE INDEX identity_changestreams_name_idx ON identity_changestreams(name);
-CREATE INDEX identity_changestreams_account_id_idx ON identity_changestreams(account_id);
-CREATE INDEX identity_changestreams_identity_source_id_idx ON identity_changestreams(identity_source_id);
-
-
 -- Trigger to track changes in the `identities` table after insert
 -- +goose StatementBegin
-CREATE TRIGGER identity_changestreams_after_insert
+CREATE TRIGGER change_streams_after_insert
 AFTER INSERT ON identities
 FOR EACH ROW
 BEGIN
-    INSERT INTO identity_changestreams (change_type, identity_id, created_at, updated_at, name, kind, account_id, identity_source_id)
-    	VALUES ('INSERT', NEW.identity_id, NEW.created_at, NEW.updated_at, NEW.name, NEW.kind, NEW.account_id, NEW.identity_source_id);
+    INSERT INTO change_streams (change_entity, change_type, change_entity_id, account_id, payload)
+		VALUES ('IDENTITY', 'INSERT', NEW.identity_id, NEW.account_id,
+				'{"identity_id": "' || NEW.identity_id || '", "created_at": "' || NEW.created_at ||
+				'", "updated_at": "' || NEW.updated_at || '", "name": "' || NEW.name ||
+				'", "kind": ' || NEW.kind || ', "account_id": ' || NEW.account_id ||
+				', "identity_source_id": "' || NEW.identity_source_id || '"}');
 END;
 -- +goose StatementEnd
 
 -- Trigger to track changes in the `identities` table after update
 -- +goose StatementBegin
-CREATE TRIGGER identity_changestreams_after_update
+CREATE TRIGGER change_streams_after_update
 AFTER UPDATE ON identities
 FOR EACH ROW
 BEGIN
     UPDATE identities SET updated_at = CURRENT_TIMESTAMP WHERE identity_id = OLD.identity_id;
-    INSERT INTO identity_changestreams (change_type, identity_id, created_at, updated_at, name, kind, account_id, identity_source_id)
-	    VALUES ('UPDATE', NEW.identity_id, NEW.created_at,CURRENT_TIMESTAMP, NEW.name, NEW.kind, NEW.account_id, NEW.identity_source_id);
+    INSERT INTO change_streams (change_entity, change_type, change_entity_id, account_id, payload)
+		VALUES ('IDENTITY', 'UPDATE', NEW.identity_id, NEW.account_id,
+				'{"identity_id": "' || NEW.identity_id || '", "created_at": "' || NEW.created_at ||
+				'", "updated_at": "' || NEW.updated_at || '", "name": "' || NEW.name ||
+				'", "kind": ' || NEW.kind || ', "account_id": ' || NEW.account_id ||
+				', "identity_source_id": "' || NEW.identity_source_id || '"}');
 END;
 -- +goose StatementEnd
 
 -- Trigger to track changes in the `identities` table after delete
 -- +goose StatementBegin
-CREATE TRIGGER identity_changestreams_after_delete
+CREATE TRIGGER change_streams_after_delete
 AFTER DELETE ON identities
 FOR EACH ROW
 BEGIN
-    INSERT INTO identity_changestreams (change_type, identity_id, created_at, updated_at, name, kind, account_id, identity_source_id)
-    	VALUES ('DELETE', OLD.identity_id, OLD.created_at, OLD.updated_at, OLD.name, OLD.kind, OLD.account_id, OLD.identity_source_id);
+    INSERT INTO change_streams (change_entity, change_type, change_entity_id, account_id, payload)
+		VALUES ('IDENTITY', 'DELETE', OLD.identity_id, OLD.account_id,
+				'{"identity_id": "' || OLD.identity_id || '", "created_at": "' || OLD.created_at ||
+				'", "updated_at": "' || OLD.updated_at || '", "name": "' || OLD.name ||
+				'", "kind": ' || OLD.kind || ', "account_id": ' || OLD.account_id ||
+				', "identity_source_id": "' || OLD.identity_source_id || '"}');
 END;
 -- +goose StatementEnd
 
 -- +goose Down
-DROP TRIGGER IF EXISTS identity_changestreams_after_insert;
-DROP TRIGGER IF EXISTS identity_changestreams_after_update;
-DROP TRIGGER IF EXISTS identity_changestreams_after_delete;
-DROP TABLE IF EXISTS identity_changestreams;
+DROP TRIGGER IF EXISTS change_streams_after_insert;
+DROP TRIGGER IF EXISTS change_streams_after_update;
+DROP TRIGGER IF EXISTS change_streams_after_delete;
 DROP TABLE IF EXISTS identities;
