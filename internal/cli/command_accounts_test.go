e951bcc32888f808af14b4d3c7fc7199dcfa7b7b
Nicola Gallo <nicolagmt@hotmail.com>
2024-08-26 00:54:25 +0200

    test: working on cli tests

---
+++ internal/cli/command_accounts_test.go
@@ -0,0 +1,45 @@
+// Copyright 2024 Nitro Agility S.r.l.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package cli
+
+import (
+	"bytes"
+	"testing"
+
+	"github.com/spf13/viper"
+	"github.com/stretchr/testify/assert"
+)
+
+// TestCreateCommandForAccounts tests the createCommandForAccounts function.
+func TestCreateCommandForAccounts(t *testing.T) {
+	assert := assert.New(t)
+	v := viper.New()
+	cmd := createCommandForAccounts(v)
+	assert.NotNil(cmd, "The command should not be nil")
+
+	var buf bytes.Buffer
+	cmd.SetOut(&buf)
+	cmd.SetArgs([]string{"-h"})
+
+	err := cmd.Execute()
+
+	assert.NoError(err)
+	output := buf.String()
+	assert.Contains(output, "The official PermGuard CLI")
+	assert.Contains(output, "Copyright © 2022 Nitro Agility S.r.l.")
+	assert.Contains(output, "This command manages accounts.")
+}
