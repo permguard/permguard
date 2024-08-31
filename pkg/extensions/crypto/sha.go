23729d90fecadc6100e34b3e5bf41cc6982b1bd2
Nicola Gallo <nicolagmt@hotmail.com>
2024-08-31 22:38:30 +0200

    refactor: updating cli to reflect the new development flow

---
+++ pkg/extensions/crypto/sha.go
@@ -0,0 +1,35 @@
+// Copyright 2024 Nitro Agility S.r.l.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package crypto
+
+import (
+	"crypto/sha1"
+	"encoding/hex"
+)
+
+// ComputeSHA1 computes the SHA1 hash of the given data.
+func ComputeSHA1(data []byte) string {
+	hasher := sha1.New()
+	hasher.Write(data)
+	hashBytes := hasher.Sum(nil)
+	return hex.EncodeToString(hashBytes)
+}
+
+// ComputeStringSHA1 computes the SHA1 hash of the given string.
+func ComputeStringSHA1(data string) string {
+	return ComputeSHA1([]byte(data))
+}
