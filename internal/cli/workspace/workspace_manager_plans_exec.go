27d5bb931ba3a4dace9f9fff47bfaba3a4dc4145
Nicola Gallo <nicolagmt@hotmail.com>
2024-09-15 12:53:17 +0200

    feat: implementing abastract language 'for' schemas

--- internal/cli/workspace/workspace_manager_plans_exec.go
+++ internal/cli/workspace/workspace_manager_plans_exec.go
@@ -50,7 +50,13 @@ func (m *WorkspaceManager) execInternalPlan(internal bool, out func(map[string]a
 	}
 
 	output := map[string]any{}
-	if m.ctx.IsVerboseJSONOutput() {
+	if m.ctx.IsVerboseTerminalOutput() {
+		out(nil, "plan", fmt.Sprintf("Head remote set to %s.", aziclicommon.KeywordText(headInfo.Remote)), nil)
+		out(nil, "plan", fmt.Sprintf("Head accountid set to %s.", aziclicommon.BigNumberText(headInfo.AccountID)), nil)
+		out(nil, "plan", fmt.Sprintf("Head remote_repo set to %s.", aziclicommon.KeywordText(headInfo.Repo)), nil)
+		out(nil, "plan", fmt.Sprintf("Head refid set to %s.", aziclicommon.IDText(headInfo.RefID)), nil)
+		out(nil, "plan", fmt.Sprintf("Repo set to %s.", aziclicommon.KeywordText(headRef)), nil)
+	} else if m.ctx.IsVerboseJSONOutput() {
 		remoteObj := map[string]any{
 			"remote":    headInfo.Remote,
 			"accountid": headInfo.AccountID,
@@ -61,19 +67,19 @@ func (m *WorkspaceManager) execInternalPlan(internal bool, out func(map[string]a
 		output = out(output, "repo", headRef, nil)
 	}
 
-	if m.ctx.IsVerboseTerminalOutput() {
-		out(nil, "plan", fmt.Sprintf("Initiating the planning process for repo %s.", aziclicommon.KeywordText(headRef)), nil)
+	if m.ctx.IsTerminalOutput() {
+		out(nil, "", fmt.Sprintf("Initiating the planning process for repo %s.", aziclicommon.KeywordText(headRef)), nil)
 	}
 
 	err = m.plan()
 	if err != nil {
-		if m.ctx.IsVerboseTerminalOutput() {
-			out(nil, "plan", fmt.Sprintf("Planning process failed for repo %s", aziclicommon.KeywordText(headRef)), nil)
+		if m.ctx.IsTerminalOutput() {
+			out(nil, "", "Planning process failed.", nil)
 		}
 		return nil, err
 	}
-	if m.ctx.IsVerboseTerminalOutput() {
-		out(nil, "plan", fmt.Sprintf("Planning process completed successfully for repo %s.", aziclicommon.KeywordText(headRef)), nil)
+	if m.ctx.IsTerminalOutput() {
+		out(nil, "", "Planning process completed successfully.", nil)
 	}
 	return output, nil
 }
