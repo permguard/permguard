b7129c2c1a7e1980499aab2f0f1c07192628b46a
Nicola Gallo <nicolagmt@hotmail.com>
2024-10-01 16:51:48 +0200

    feat: working on the statemachine integration over grpc transport layer

---
+++ internal/cli/workspace/workspace_manager_statemachine_clients.go
@@ -0,0 +1,63 @@
+// Copyright 2024 Nitro Agility S.r.l.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package workspace
+
+import (
+	notppackets "github.com/permguard/permguard-notp-protocol/pkg/notp/packets"
+	notpstatemachines "github.com/permguard/permguard-notp-protocol/pkg/notp/statemachines"
+	notpsmpackets "github.com/permguard/permguard-notp-protocol/pkg/notp/statemachines/packets"
+)
+
+// OnPushSendNotifyCurrentState notifies the current state.
+func (m *WorkspaceManager) OnPushSendNotifyCurrentState(handlerCtx *notpstatemachines.HandlerContext, statePacket *notpsmpackets.StatePacket, packets []notppackets.Packetable) (*notpstatemachines.HostHandlerRuturn, error) {
+	handlerReturn := &notpstatemachines.HostHandlerRuturn{
+		Packetables: packets,
+	}
+	return handlerReturn, nil
+}
+
+// OnPushHandleNotifyCurrentStateResponse handles the current state response.
+func (m *WorkspaceManager) OnPushHandleNotifyCurrentStateResponse(handlerCtx *notpstatemachines.HandlerContext, statePacket *notpsmpackets.StatePacket, packets []notppackets.Packetable) (*notpstatemachines.HostHandlerRuturn, error) {
+	handlerReturn := &notpstatemachines.HostHandlerRuturn{
+		Packetables: packets,
+	}
+	return handlerReturn, nil
+}
+
+// OnPushHandleNegotiationRequest handles the negotiation request.
+func (m *WorkspaceManager) OnPushHandleNegotiationRequest(handlerCtx *notpstatemachines.HandlerContext, statePacket *notpsmpackets.StatePacket, packets []notppackets.Packetable) (*notpstatemachines.HostHandlerRuturn, error) {
+	handlerReturn := &notpstatemachines.HostHandlerRuturn{
+		Packetables: packets,
+	}
+	return handlerReturn, nil
+}
+
+// OnPushSendNegotiationResponse sends the negotiation response.
+func (m *WorkspaceManager) OnPushSendNegotiationResponse(handlerCtx *notpstatemachines.HandlerContext, statePacket *notpsmpackets.StatePacket, packets []notppackets.Packetable) (*notpstatemachines.HostHandlerRuturn, error) {
+	handlerReturn := &notpstatemachines.HostHandlerRuturn{
+		Packetables: packets,
+	}
+	return handlerReturn, nil
+}
+
+// OnPushExchangeDataStream exchanges the data stream.
+func (m *WorkspaceManager) OnPushExchangeDataStream(handlerCtx *notpstatemachines.HandlerContext, statePacket *notpsmpackets.StatePacket, packets []notppackets.Packetable) (*notpstatemachines.HostHandlerRuturn, error) {
+	handlerReturn := &notpstatemachines.HostHandlerRuturn{
+		Packetables: packets,
+	}
+	return handlerReturn, nil
+}
