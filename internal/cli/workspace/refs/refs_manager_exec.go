2d3c0ff01618e01a2002d95412c6ead120f05206
Nicola Gallo <nicolagmt@hotmail.com>
2024-11-14 16:51:32 +0100

    feat: working on heads

--- internal/cli/workspace/refs/refs_manager_exec.go
+++ internal/cli/workspace/refs/refs_manager_exec.go
@@ -43,7 +43,7 @@ func (m *RefManager) ExecCheckoutHead(remote string, accountID int64, repo strin
 	if output == nil {
 		output = map[string]any{}
 	}
-	ref := azicliwkscommon.GenerateRef(remote, accountID, repoID)
+	ref := azicliwkscommon.GenerateRef(false, remote, accountID, repoID)
 	err := m.SaveRefConfig(repoID, ref, commit)
 	if err != nil {
 		return nil, output, err
