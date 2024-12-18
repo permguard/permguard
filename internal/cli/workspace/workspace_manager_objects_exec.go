5bb3962423a422f7d109b288dad1810d077326d3
Nicola Gallo <nicolagmt@hotmail.com>
2024-12-19 00:23:24 +0100

    feat: working on cedar integration

--- internal/cli/workspace/workspace_manager_objects_exec.go
+++ internal/cli/workspace/workspace_manager_objects_exec.go
@@ -276,7 +276,6 @@ func (m *WorkspaceManager) ExecHistory(out aziclicommon.PrinterOutFunc) (map[str
 				commitStr := m.getCommitString(commitInfo.GetCommitOID(), commit)
 				out(nil, "", commitStr, nil, true)
 			}
-			out(nil, "", "\n", nil, false)
 		}
 	} else if m.ctx.IsJSONOutput() {
 		objMaps := []map[string]any{}
