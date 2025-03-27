5d28104b3dc528384a3be9cc1f31f85cc4c4d278
Nicola Gallo <nicolagmt@hotmail.com>
2025-03-27 23:31:46 +0100

    refactor: feat: improved the init params

--- internal/cli/workspace/workspace_manager_core_exec.go
+++ internal/cli/workspace/workspace_manager_core_exec.go
@@ -72,19 +72,21 @@ func (m *WorkspaceManager) ExecInitWorkspace(initParams *InitParms, out aziclico
 		firstInit = false
 	}
 
+	wksName := ""
 	if initParams != nil {
-		manifest, err := azztas.NewManifest(initParams.Name)
-		if err != nil {
-			return failedOpErr(nil, err)
-		}
-		_, manifestData, err := azztas.ValidateManifest(manifest, true)
-		if err != nil {
-			return failedOpErr(nil, err)
-		}
-		_, err = m.persMgr.WriteFileIfNotExists(azicliwkspers.WorkspaceDir, azztas.ManifestFileName, manifestData, 0644, false)
-		if err != nil {
-			return failedOpErr(nil, err)
-		}
+		wksName = initParams.Name
+	}
+	manifest, err := azztas.NewManifest(wksName)
+	if err != nil {
+		return failedOpErr(nil, err)
+	}
+	_, manifestData, err := azztas.ValidateManifest(manifest, true)
+	if err != nil {
+		return failedOpErr(nil, err)
+	}
+	_, err = m.persMgr.WriteFileIfNotExists(azicliwkspers.WorkspaceDir, azztas.ManifestFileName, manifestData, 0644, false)
+	if err != nil {
+		return failedOpErr(nil, err)
 	}
 
 	fileLock, err := m.tryLock()
