4cbbaac86406559bfd858ceac9c4d1be75edf8f8
Nicola Gallo <nicolagmt@hotmail.com>
2025-04-15 15:29:35 +0200

    refactor: docs: updated docs

--- internal/cli/porcelaincommands/workspace/command_workspace_history.go
+++ internal/cli/porcelaincommands/workspace/command_workspace_history.go
@@ -53,9 +53,6 @@ func runECommandForHistoryWorkspace(deps azcli.CliDependenciesProvider, cmd *cob
 	}
 	output, err := wksMgr.ExecHistory(outFunc(ctx, printer))
 	if err != nil {
-		if ctx.IsNotVerboseTerminalOutput() {
-			printer.Println("Failed to show hisotry.")
-		}
 		if ctx.IsVerboseTerminalOutput() || ctx.IsJSONOutput() {
 			sysErr := azerrors.WrapHandledSysErrorWithMessage(azerrors.ErrCliOperation, "failed to show history.", err)
 			printer.Error(sysErr)
